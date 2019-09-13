96454578c9e886baf40ca481103b99ab3f4475bd
OdysseyCreations <47780708+OdysseyCreations@users.noreply.github.com>
2019-09-13 07:27:43 -0700

    Update Val_ptr.hpp

--- src/Val_ptr.hpp
+++ src/Val_ptr.hpp
@@ -7,9 +7,32 @@
 
 namespace odm
 {
+	/**
+	 * Pointer to the vector 2 value
+	 * @param v The vector whose value is to be pointed
+	 * @return Float pointer to the vector elem
+	*/
 	__forceinline static const float* value_ptr(const Vector2f& v);
+	
+	/**
+	 * Pointer to the vector 3 value
+	 * @param v The vector whose value is to be pointed
+	 * @return Float pointer to the vector elem
+	*/
 	__forceinline static const float* value_ptr(const Vector3f& v);
+	
+	/**
+	 * Pointer to the vector 4 value
+	 * @param v The vector whose value is to be pointed
+	 * @return Float pointer to the vector elem
+	*/
 	__forceinline static const float* value_ptr(const Vector4f& v);
+	
+	/**
+	 * Pointer to the matrix 4x4 value
+	 * @param v The vector whose value is to be pointed
+	 * @return Float pointer to the vector elem
+	*/
 	__forceinline static const float* value_ptr(const Matrix4x4& m);
 }
 
