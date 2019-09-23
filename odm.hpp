fdaa2ed0d7f324d7d4696df996ee29c8a54eeefb
OdysseyGameWorks <55286413+OdysseyGameWorks@users.noreply.github.com>
2019-09-22 22:40:56 -0700

    Create odm.hpp

---
+++ odm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Vector2f.hpp"
+#include "Vector3f.hpp"
+#include "Vector4f.hpp"
+#include "Matrix4x4.hpp"
+#include "Color.hpp"
+#include "Val_ptr.hpp"
+#include "Transform.hpp"
+#include "Transform_mat.hpp"
+#include "View_mat.hpp"
